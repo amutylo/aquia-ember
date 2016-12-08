81e4ca7b2c48c6a4c2fb88973e20dc528b56601d
Andrii Mutylo <amutylo@gmail.com>
2016-12-08 22:39:27 +0200

    fix adapter bug

--- app/adapters/application.js
+++ app/adapters/application.js
@@ -2,5 +2,25 @@ import DS from 'ember-data';
 
 export default DS.JSONAPIAdapter.extend({
   host: 'http://dublin-drupal.lc',
-  namespace: 'api'
+  namespace: 'api',
+
+  pathForType(type) {
+    let entityPath;
+    switch(type) {
+      case 'node--article':
+        entityPath = 'node/article';
+        break;
+      case 'node--page':
+        entityPath = 'node/page';
+        break;
+      case 'user--user':
+        entityPath = 'user/user';
+        break;
+    }
+    return entityPath;
+  },
+
+  buildURL() {
+    return this._super(...arguments) + '?_format=api_json';
+  }
 });
